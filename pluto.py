95ea7fab66cf95b13d1fa93107874be5692e8aa2
geremachek <mrender2005@gmail.com>
2018-11-02 23:40:20 -0700

    Update pluto.py

--- pluto.py
+++ pluto.py
@@ -20,6 +20,9 @@ comment = False
 file_color=0
 dir_color=13
 sel_fmt=A_REVERSE
+exit_key="q"
+del_key="d"
+home_key="h"
 
 if os.path.isfile(".plutorc"):
     with io.open(".plutorc", "r") as config:
@@ -40,7 +43,28 @@ if os.path.isfile(".plutorc"):
                         sel_fmt=A_REVERSE
                     elif cval.rstrip() == "underline":
                         sel_fmt=A_UNDERLINE
-
+                    elif cval.rstrip() == "dim":
+                        sel_fmt=A_DIM
+                elif cvar == "exit":
+                    exit_key = str(cval.rstrip())
+                elif cvar == "del":
+                    del_key = str(cval.rstrip())
+                elif cvar == "home":
+                    home_key = str(cval.rstrip())
+                elif cvar == "theme":
+                    if os.path.isdir(home + "/.pluto/themes"):
+                        chdir(home + "/.pluto/themes")
+                        with io.open(cval.rstrip() + ".plut", "r") as theme:
+                            for line in theme:
+                                if line[0] == "#" or line.isspace() or len(line) == 0:
+                                    continue
+                                elif ":" in list(line) and "#" not in list(line):
+                                    cvar = line.split(":")[0]
+                                    cval = line.split(":")[1]
+                                    if cvar == "file":
+                                        file_color = int(cval)
+                                    elif cvar == "dir":
+                                        dir_color = int(cval)
             else:
                 endwin()
                 print("\033[101mError -->\033[0m " + "\033[1m" + line.rstrip() + "\033[0m is invalid")
@@ -73,6 +97,13 @@ count=0
 sc1 = my-2
 sc2 = 0
 
+while index < len(cfiles):
+    files = cfiles[index]
+    if files[0] == ".":
+        cfiles.pop(cfiles.index(files))
+    else:
+        index += 1
+
 for files in cfiles:
     if count != my-2:
         count += 1
@@ -99,7 +130,7 @@ cy, cx = 1, 1
 
 while True:
     key = s.getch()
-    if key == ord("q"):
+    if key == ord(exit_key):
         endwin()
         exit()
     elif key == KEY_DOWN:
@@ -327,11 +358,131 @@ while True:
         else:
             s.addstr(cfiles[0], color_pair(file_color) + sel_fmt)
             s.move(1,1)
-    elif key == ord("d"):
+    elif key == ord(del_key):
         if scrolled == False:
             system("rm -rf " + cfiles[cfile])
+            s.clear()
+            cwd = getcwd()
+            cfiles.clear()
+            cfiles = listdir(cwd)
+            
+            index=0
+    
+            while index < len(cfiles):
+                files = cfiles[index]
+                if files[0] == ".":
+                    cfiles.pop(cfiles.index(files))
+                else:
+                    index += 1
+
+            count=0
+
+            for files in cfiles:
+                if count != my-2:
+                    count += 1
+                    s.move(count, 1)
+                    if os.path.isdir(files):
+                        s.addstr(files, color_pair(dir_color))
+                    else:
+                        s.addstr(files, color_pair(file_color))
+                    s.move(count,1)
+                else:
+                    break
+            
+            cfile = 0
+            cy, cx = 1, 1
+            s.move(1,1)           
+            if os.path.isdir(cfiles[cfile]):
+                s.addstr(cfiles[cfile], color_pair(dir_color) + sel_fmt)
+                s.move(cy,cx)
+            else:
+                s.addstr(cfiles[cfile], color_pair(file_color) + sel_fmt)
+                s.move(cy,cx)
         else:
             system("rm -rf " + new[cfile])
+            s.clear()
+            cwd = getcwd()
+            cfiles.clear()
+            cfiles = listdir(cwd)
+            
+            index=0
+    
+            while index < len(cfiles):
+                files = cfiles[index]
+                if files[0] == ".":
+                    cfiles.pop(cfiles.index(files))
+                else:
+                    index += 1
+
+            count=0
+            
+            new = cfiles[sc2:sc1]
+
+            for files in new:
+                if count != my-2:
+                    count += 1
+                    s.move(count, 1)
+                    if os.path.isdir(files):
+                        s.addstr(files, color_pair(dir_color))
+                    else:
+                        s.addstr(files, color_pair(file_color))
+                    s.move(count,1)
+                else:
+                    break
+            
+            cy, cx = 1, 1
+            s.move(1,1)
+            if os.path.isdir(new[cfile]):
+                s.addstr(new[cfile], color_pair(dir_color) + sel_fmt)
+                s.move(cy,cx)
+            else:
+                s.addstr(new[cfile], color_pair(file_color) + sel_fmt)
+                s.move(cy,cx)
+
+    elif key == ord(home_key):
+        scrolled = False
+        s.clear()
+        s.refresh()
+        chdir(home)
+        cwd = getcwd()
+        cfiles.clear()
+        cfiles = listdir(cwd)
+        cfile = 0
+        cy, cx = 1, 1
+        s.move(1,1)            
+
+        count=0
+
+        index=0
+
+        while index < len(cfiles):
+            files = cfiles[index]
+            if files[0] == ".":
+                cfiles.pop(cfiles.index(files))
+            else:
+                index += 1
+
+        for files in cfiles:
+            if count != my-2:
+                count += 1
+                s.move(count, 1)
+                if os.path.isdir(files):
+                    s.addstr(files, color_pair(dir_color))
+                else:
+                    s.addstr(files, color_pair(file_color))
+                s.move(count,1)
+            else:
+                break
+            
+        s.move(1,1)            
+
+        if os.path.isdir(cfiles[0]):
+            s.addstr(cfiles[0], color_pair(dir_color) + sel_fmt)
+            s.move(1,1)
+        else:
+            s.addstr(cfiles[0], color_pair(file_color) + sel_fmt)
+            s.move(1,1)
+
     else:
         continue
 endwin()
