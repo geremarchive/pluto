149c809a0808b71e62504b2dcc25c34615cf2b39
geremachek <mrender2005@gmail.com>
2019-02-10 14:20:20 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/geremachek/pluto/master/pluto.bin > pluto.bin
+curl https://raw.githubusercontent.com/geremachek/pluto/master/config > config
+chmod +x pluto.bin
+
+sudo mv pluto.bin /usr/bin/pluto
+
+if [ -d $HOME/.config ]
+then
+	mkdir $HOME/.config
+fi
+
+mkdir $HOME/.config/pluto
+mv config $HOME/.config/pluto/config
+
+printf "\033[1;32mPluto has been installed!\033[0m\n"
